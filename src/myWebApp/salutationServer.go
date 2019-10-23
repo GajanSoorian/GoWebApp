a2c14fcc32ccade68d29ccaa91e6a506c6348769
CheesyPizza <gajansuryan@gmail.com>
2019-10-23 14:13:12 -0400

    Create salutationServer.go
    Local server two two router functions for sayHello and say Bye

---
+++ src/myWebApp/salutationServer.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"net/http"
+	"log"
+)
+
+func sayHello(w http.ResponseWriter ,r *http.Request) {
+	r.ParseForm()
+	fmt.Printf(" Type of r = %T  and value = %V",r,r)
+	fmt.Println("Form =",r.Form)
+	fmt.Println(
+		"URL Path = ",
+		r.URL.Path)
+	fmt.Println("URL scheme = ",r.URL.Scheme)
+	fmt.Println("url long form = ", r.Form["url_long"])
+	for k,v :=range r.Form{
+		fmt.Println("key:", k)
+        fmt.Println("val:", strings.Join(v, ""))
+	}
+
+	fmt.Println("Going to send a hello response to the client")
+	fmt.Fprintf(w, "Well Hello There!") // send data to client side
+}
+
+
+func sayBye(w http.ResponseWriter ,r *http.Request) {
+
+	fmt.Println("Going to say bye to the client")
+	fmt.Fprintf(w, "Ok Byeee!") // send data to client side
+}
+
+func main() {
+	http.HandleFunc("/sayHello", sayHello) // set router
+	http.HandleFunc("/sayBye", sayBye) // set router
+    err := http.ListenAndServe(":9090", nil) // set listen port
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
